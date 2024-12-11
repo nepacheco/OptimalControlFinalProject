04883378cd82168aacda242e6ead5ee2c1bd0a82
Pacheco <nepacheco@wpi.edu>
2024-12-11 12:30:28 -0500

    Added a new way to calculate the state

--- get_state.m
+++ get_state.m
@@ -8,7 +8,7 @@ arguments
 end
 
 board.remove_shape();
-board.check_done();
+% board.check_done();
 if board.done
     state = -1;
 else
@@ -39,6 +39,11 @@ else
             state = elems + shape + holes + prev_shape;
             
         case 5
+            % the state is given by looking at the top two rows of the
+            % board and treating those rows as a binary number. 1 if they
+            % have an object and 0 otherwise. We then can also have 7
+            % shapes so there are a total of 2^(2*cols)*7 total
+            % possible states
             [row,~] = find(board.board);
             elems = [];
             if ~isempty(row)
@@ -49,12 +54,40 @@ else
                     elems = find(board.board(min_row:min_row+1,:));
                 end
             end
-            state = 0;
+            state = 1;
             for i = elems'
                 state = state + 2^(i-1);
             end
-            shape = board.shape.id*size(board.board,2);
-            state = state*7 + shape;
+            shape = board.shape.id*size(board.board,2) + 1; 
+            state = state*7 + shape;% shape ID goes from 0 - 6 so we add 1 to not multiply by 0
+        case 6
+            % For this state we look at the highest three rows and then
+            % add the peak height in each column. Each column can have a
+            % value between 0-3. 
+            % This gives a total of 4^(numCol)*7 total states because of
+            % the 7 pieces. Same number of states as case 5, but with
+            % potentially slightly richer information.
+            [row,~] = find(board.board);
+            elems = [];
+            smallBoard = zeros(4,size(board.board,2));
+            if ~isempty(row)
+                min_row = min(row);
+                if ((size(board.board,1)-2) < min_row) % minimum row is within 4 from bottom
+                    smallBoard = board.board((size(board.board,1)-2):size(board.board,1),:); % Take bottom four rows of the board
+                else % min row is a value between 0 and 16
+                    smallBoard = board.board(min_row:min_row+2,:);
+                end
+            end
+            % just so that find will return 3 for the row if the column is filled
+            smallBoard = flip(smallBoard,1);
+            state = 0;
+            for i = 1:size(board.board,2)
+                % elems will either be empty (0) or a value between 1-3
+                elems = find(smallBoard(:,i),1,'last'); 
+                if ~isempty(elems)
+                    state = state + (elems*4^(i-1));
+                end
+            end
         otherwise
             state = 0;
     end
